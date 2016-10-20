54ed1254cf23f3a31d620aa4a7c7ba882e9b40b9
John Haney <John.Haney@txdot.gov>
2016-10-20 17:17:00 -0500

    responsive title changes

--- js/map.js
+++ js/map.js
@@ -7,7 +7,7 @@ var zoomHome = L.Control.zoomHome();
 
 var layerLabels;
 
-  function setBasemap(basemap) {
+function setBasemap(basemap) {
     if (layer) {
       map.removeLayer(layer);
     }
@@ -35,42 +35,42 @@ var layerLabels;
     }
   }
 
-  function changeBasemap(basemaps){
+function changeBasemap(basemaps){
     var basemap = basemaps.value;
     setBasemap(basemap);
   }
   
-  //load AUS district boundary AGO service
-  L.esri.featureLayer({
+//load AUS district boundary AGO service
+L.esri.featureLayer({
     url: 'https://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/AUS_District_Boundary_Ln/FeatureServer/0',
     style: function (feature) {
         return {color: '#000000', weight: 3};
     }
   }).addTo(map);
     
-  //load AUS strategic corridors AGO service
-  //L.esri.featureLayer({
-    //  url: 'http://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/AUS_Strategic_Corridors_20160823/FeatureServer/0',
-    //style: function (feature) {
-      //  return {color: 'black', weight: 2, opacity: 0.5}
+//load AUS strategic corridors AGO service
+//L.esri.featureLayer({
+//  url: 'http://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/AUS_Strategic_Corridors_20160823/FeatureServer/0',
+//style: function (feature) {
+    //  return {color: 'black', weight: 2, opacity: 0.5}
   //}
-  //}).addTo(map);
+//}).addTo(map);
 
-    //load aus projects from project tracker ago feature service
-    L.esri.featureLayer({
-        url: 'https://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/TxDOT_Projects/FeatureServer/0',
-        where: "DISTRICT_NAME = 'Austin'",
-        style: function (feature) {
-            if(feature.properties.PRJ_STATUS === 'Construction Scheduled'){
-                return {color: '#d7191c', weight: 3};
-            }   else if(feature.properties.PRJ_STATUS === 'Finalizing for Construction'){
-                return {color: '#fdae61', weight: 3};
-            }   else if(feature.properties.PRJ_STATUS === 'Under Development'){
-                return {color: '#80cdc1', weight: 3};
-            }   else if(feature.properties.PRJ_STATUS === 'Long Term Planning'){
-                return {color: '#2c7bb6', weight: 3};
-            }   else {
-                return {color: '#aaaaaa', weight: 3};
-            }
+//load aus projects from project tracker ago feature service
+L.esri.featureLayer({
+    url: 'https://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/TxDOT_Projects/FeatureServer/0',
+    where: "DISTRICT_NAME = 'Austin'",
+    style: function (feature) {
+        if(feature.properties.PRJ_STATUS === 'Construction Scheduled'){
+            return {color: '#d7191c', weight: 3};
+        }   else if(feature.properties.PRJ_STATUS === 'Finalizing for Construction'){
+            return {color: '#fdae61', weight: 3};
+        }   else if(feature.properties.PRJ_STATUS === 'Under Development'){
+            return {color: '#80cdc1', weight: 3};
+        }   else if(feature.properties.PRJ_STATUS === 'Long Term Planning'){
+            return {color: '#2c7bb6', weight: 3};
+        }   else {
+            return {color: '#aaaaaa', weight: 3};
         }
+      }
     }).addTo(map);
